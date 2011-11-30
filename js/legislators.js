afd1f3f6210cc2bc05bcfafaeb8e1ccde24ea682
Pete Butler <pbutler@basdesign.com>
2011-11-30 10:45:54 -0800

    added thumbnail

--- js/legislators.js
+++ js/legislators.js
@@ -42,17 +42,30 @@ $(function(){
       this.model.set(data);
     }
   });
-  
+
   var Legislator = Backbone.Model.extend({
     initialize: function(){
+      // FACEBOOK
+      //  TODO: this should be getting a model, not setting this stuff directly
       var avatar_url = "http://graph.facebook.com/"+this.get("facebook_id")+"/picture";
       var wall_url = "http://www.facebook.com/"+this.get("facebook_id");
       this.set({"fb":{"avatar_url": avatar_url, "wall_url": wall_url}});
-      console.log("created legislator: ", this.toJSON())
+      // TWITTER
+      // extend the legislators model with the models returned from the TweetsList collection
+      if(this.get("twitter_id") !== "" ){
+        var tweets = new TweetsList({"twitter_id":this.get("twitter_id")});
+        var l = this;
+        tweets.bind("all", function(){
+          l.set({"tweets":tweets.toJSON()});
+          console.log("Legislator tweets changed: ",l.toJSON().tweets);
+          l.change();
+        });
+      }
     }
   });
   
   var LegislatorsList = Backbone.Collection.extend({
+    model: Legislator,
     url: function(){
       for(var i in sunlightParams){
         if(sunlightParams[i] === "undefined" || sunlightParams[i] === ""){
@@ -83,15 +96,48 @@ $(function(){
   });
 
   var LegislatorView = Backbone.View.extend({
+    model: Legislator,
     template: _.template($('#legislator_template').html()),
-    initialize: function(){
+    initialize: function() {
       this.render();
+      this.model.bind('change', function(){
+      console.log("View changed")
+      });
+    },
+    changed: function(){
     },
     render: function() {
       var l = this.attributes;
-      console.log("view: ",l)
       $("#legislators").append(this.template(l));
+      console.log("LegislatorView: ",l, ", tweets: ",l.tweets)
       return this;
+    }
+  });
+
+  var Tweet = Backbone.Model.extend({
+    initialize: function(){
+      if(this.get("user") !== undefined){
+ //       console.log("New Tweet @" + this.get("user").name + ": " + this.get("text"));
+      }
+    }
+  });
+
+  var TweetsList = Backbone.Collection.extend({
+    model: Tweet,
+    url: function(){
+      var baseUrl = "https://api.twitter.com/1/statuses/user_timeline.json";
+      var data =  {
+        include_entities: true,
+        exclude_replies: true,
+        include_rts: true,
+        count: 3,
+        screen_name: (this.toJSON()[0].twitter_id)
+      };
+      return baseUrl + "?callback=?&" + $.param(data); // jsonp here
+    },
+    initialize: function(){
+      // console.log("Tweets initialized with id: ",this.toJSON()[0].twitter_id);
+      this.fetch();
     }
   });
 
